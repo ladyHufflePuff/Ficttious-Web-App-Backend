866f4f7f9e18978b60b327fc2f08058cf30d1348
Uchechi Johnson <uchechijohnson35@gmail.com>
2024-11-17 14:17:09 +0400

    Connect to database

---
+++ db.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb')
+const connectionString = 'mongodb+srv://uchechi:3iyjkHSUkXy2Qndh@cluster0.hymz6hv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+let dbConnection
+
+module.exports = {
+    connectToDb:(callback) => {
+        MongoClient.connect(connectionString)
+            .then((client) => {
+                dbConnection = client.db('Learn-Hub-App')
+                console.log("database connected")
+                return callback()
+            })
+            .catch(err => {
+                console.log(err)
+                return callback(err)
+            })
+    },
+    getDb: () => dbConnection
+}
